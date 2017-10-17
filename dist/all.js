ca9e14f4afbbae5397221d589001359f97775566
Muslim Guseinov <gusejnov1999@list.ru>
2017-10-17 14:09:27 +0300

    Compile babel file from src/js/*

---
+++ dist/all.js
@@ -0,0 +1,8 @@
+"use strict";
+
+var _templateObject = _taggedTemplateLiteral(["hello world"], ["hello world"]);
+
+function _taggedTemplateLiteral(strings, raw) { return Object.freeze(Object.defineProperties(strings, { raw: { value: Object.freeze(raw) } })); }
+
+console.log(_templateObject);
+//# sourceMappingURL=all.js.map
