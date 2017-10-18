c16ccdc29d99b73e52d200e666e7a0af78790e3d
Muslim Guseinov <gusejnov1999@list.ru>
2017-10-18 19:12:05 +0300

    Add material in dist

--- src/js/materialElem.js
+++ src/js/materialElem.js
@@ -14,4 +14,26 @@
           });
      }
    popup();
+
+   const notify = () => {
+    let snackbarContainer = document.querySelector('#demo-snackbar-example');
+    let showSnackbarButton = document.querySelector('#demo-show-snackbar');
+    let handler = function(event) {
+      showSnackbarButton.style.backgroundColor = '';
+    };
+    showSnackbarButton.addEventListener('click', function() {
+      'use strict';
+      showSnackbarButton.style.backgroundColor = '#' +
+          Math.floor(Math.random() * 0xFFFFFF).toString(16);
+      var data = {
+        message: 'Button color changed.',
+        timeout: 2000,
+        actionHandler: handler,
+        actionText: 'Undo'
+      };
+      snackbarContainer.MaterialSnackbar.showSnackbar(data);
+    });
+   }
+
+   notify();
 })()
