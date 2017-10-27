17296cfe630dc29e3c8700a33590e8c2b4fa3097
Muslim Guseinov <gusejnov1999@list.ru>
2017-10-27 18:24:10 +0300

    Fix localStorage

--- src/js/app.js
+++ src/js/app.js
@@ -431,8 +431,8 @@
       
       let localGetPen     = localStorage.getItem(`colorPen`);
       ctx.strokeStyle     = localGetPen;
-      colorPenInput.value = `#${getHexRGBColor(localGetPen)}`;
-
+      colorPenInput.value = getHexRGBColor(localGetPen);
+      
       /**
        * @event input 
        * Change background-color canvas
@@ -455,23 +455,21 @@
       {
         color = color.replace(/\s/g,"");
         var aRGB = color.match(/^rgb\((\d{1,3}[%]?),(\d{1,3}[%]?),(\d{1,3}[%]?)\)$/i);
-       
+      
         if(aRGB)
         {
-          color = ``;
-          for (var i=1;  i<=3; i++) color += Math.round((aRGB[i][aRGB[i].length-1]=="%"?2.55:1)*parseInt(aRGB[i])).toString(16).replace(/^(.)$/,`0$1`);
+          color = '';
+          for (var i=1;  i<=3; i++) color += Math.round((aRGB[i][aRGB[i].length-1]=="%"?2.55:1)*parseInt(aRGB[i])).toString(16).replace(/^(.)$/,'0$1');
         }
-        else color = color.replace(/^#?([\da-f])([\da-f])([\da-f])$/i, `$1$1$2$2$3$3`);
-         
+        else color = color.replace(/^#?([\da-f])([\da-f])([\da-f])$/i, '$1$1$2$2$3$3');
+               
         return color;
       }
       
 
-      let localGetBg               = localStorage.getItem(`colorBg`);
+      let localGetBg               =    localStorage.getItem(`colorBg`);
       canvas.style.backgroundColor = localGetBg;
       colorBgInput.value           = `#${getHexRGBColor(localGetBg)}`;
-      
-      localStorage.setItem(`colorPen`, canvas.style.backgroundColor)
       
     //   MySelect.onchange = () => {
     //      let lineJoinValue = document.getElementById("MySelect").value;
