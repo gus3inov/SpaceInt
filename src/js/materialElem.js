453cb27836cc6a289126d0e6cdfae4404ec7c2b2
Muslim Guseinov <gusejnov1999@list.ru>
2017-10-31 18:59:03 +0300

    Uncommenct popup

--- src/js/materialElem.js
+++ src/js/materialElem.js
@@ -3,15 +3,15 @@
      * @function popup 
      * Modal popup
      */
-    //  (()=>{
-    //   let dialog = document.querySelector(`dialog`);
+     (()=>{
+      let dialog = document.querySelector(`dialog`);
       
-    //   setTimeout(()=> dialog.showModal(), 500);
+      setTimeout(()=> dialog.showModal(), 500);
 
-    //   dialog.querySelector(`#closePopup`).addEventListener(`click`, function() {
-    //       dialog.close();
-    //     });
-    //  })()
+      dialog.querySelector(`#closePopup`).addEventListener(`click`, function() {
+          dialog.close();
+        });
+     })()
 
      
        (()=>{
@@ -33,5 +33,4 @@
         };
 
        })()
-
 })()
