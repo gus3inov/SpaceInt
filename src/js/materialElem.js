610dc4be92b8cf84c73af629eac5f24d4b77949d
Muslim Guseinov <gusejnov1999@list.ru>
2017-10-31 18:29:08 +0300

    Event for open and close figure menu

--- src/js/materialElem.js
+++ src/js/materialElem.js
@@ -3,14 +3,35 @@
      * @function popup 
      * Modal popup
      */
-     (()=>{
-      let dialog = document.querySelector('dialog');
+    //  (()=>{
+    //   let dialog = document.querySelector(`dialog`);
       
-      setTimeout(()=> dialog.showModal(), 500);
+    //   setTimeout(()=> dialog.showModal(), 500);
 
-      dialog.querySelector('#closePopup').addEventListener('click', function() {
-          dialog.close();
-        });
-     })()
-  
+    //   dialog.querySelector(`#closePopup`).addEventListener(`click`, function() {
+    //       dialog.close();
+    //     });
+    //  })()
+
+     
+       (()=>{
+         
+        const openMenu = document.querySelector(`#openFiguresBar`);
+        const closeMenu = document.querySelector(`#closeFiguresBar`);
+        const Menu = document.querySelector(`.bottom-toolbar__figures_menu`);
+ 
+         openMenu.onclick = () => {
+           if(Menu){
+             Menu.classList.add(`open`);
+           }
+         };
+
+         closeMenu.onclick = () => {
+          if(Menu){
+            Menu.classList.remove(`open`);
+          }
+        };
+
+       })()
+
 })()
